46037447aa77ac0a1ad22e1cde3fa23e86514e97
FrancoPalacio <francopalacio03@gmail.com>
2023-04-27 15:28:02 -0300

    primer commit

---
+++ js/app.js
@@ -0,0 +1,44 @@
+
+function add(){
+    const forma=document.getElementById("forma");
+    let operandoA=forma["operandoA"];
+    let operandoB=forma["operandoB"];
+    let resultado=parseInt(operandoA.value)+ parseInt(operandoB.value); 
+    if(isNaN(resultado))
+        resultado="Operandos vacios";
+    document.getElementById("resultado").innerHTML="resultado: "+resultado;
+       
+}
+
+function substract(){
+    const forma=document.getElementById("forma");
+    let operandoA=forma["operandoA"];
+    let operandoB=forma["operandoB"];
+    let resultado=parseInt(operandoA.value)- parseInt(operandoB.value); 
+    if(isNaN(resultado))
+        resultado="Operandos vacios";
+    document.getElementById("resultado").innerHTML="resultado: "+resultado;
+       
+}
+
+function multiply(){
+    const forma=document.getElementById("forma");
+    let operandoA=forma["operandoA"];
+    let operandoB=forma["operandoB"];
+    let resultado=parseInt(operandoA.value)* parseInt(operandoB.value); 
+    if(isNaN(resultado))
+        resultado="Operandos vacios";
+    document.getElementById("resultado").innerHTML="resultado: "+resultado;
+       
+}
+
+function divide(){
+    const forma=document.getElementById("forma");
+    let operandoA=forma["operandoA"];
+    let operandoB=forma["operandoB"];
+    let resultado=parseInt(operandoA.value)/ parseInt(operandoB.value); 
+    if(isNaN(resultado))
+        resultado="Operandos vacios";
+    document.getElementById("resultado").innerHTML="resultado: "+resultado;
+       
+}
